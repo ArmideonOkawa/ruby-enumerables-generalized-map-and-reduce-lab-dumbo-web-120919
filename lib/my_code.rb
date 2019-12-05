f8c5a2cfd7e164c071d5a8855ecc94bed75f85cc
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-12-05 05:55:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,14 @@ def map(array)
 end
 
 def reduce(array, sv = nil)
+  if sv 
+    num1 = sv
+    counter = 0 
+  else
+    num1 = array[0]
+    counter = 1 
+  end
   
+  while counter < array.length 
+    yield(num1)
 end
